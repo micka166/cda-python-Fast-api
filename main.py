e601303b0af7dfd97b4479503ab42d253070c6ef
enfrein <enfreinmickael42@gmail.com>
2024-02-09 18:45:48 +0100

    push formulaire connexion fonctionne

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from crypt import methods
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request , Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -16,16 +15,19 @@ fake_user = {"username": "admin", "password": "password"}
 async def read_root(request: Request):
     return templates.TemplateResponse("login.html", {"request": request, "login_error": None})
 
-@app.post("/user", response_class=HTMLResponse)
-async def login(request: Request, username: str, password: str):
+
+@app.post("/login", response_class=HTMLResponse)
+async def login(request: Request, username: str = Form(...), password: str = Form(...)):
     if username == fake_user["username"] and password == fake_user["password"]:
-        return RedirectResponse(url="/success")
+        return RedirectResponse(url="/success", status_code=303)
     else:
         return templates.TemplateResponse("login.html", {"request": request, "login_error": "Invalid username or password"})
+    
 
 @app.get("/success", response_class=HTMLResponse)
-async def success():
-    return "<h1>Login successful</h1>"
+async def success(request: Request):
+    return templates.TemplateResponse("user.html", {"request": request, "username": fake_user["username"]})
+
 
 @app.get("/error", response_class=HTMLResponse)
 async def error():
